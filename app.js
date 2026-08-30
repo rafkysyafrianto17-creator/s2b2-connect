00233c629502308e81318c561c6a0e84d51809e6
rafkysyafrianto17-creator <rafky.syafrianto17@sma.belajar.id>
2026-08-30 15:19:23 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const SUPABASE_URL = "https://qbqabvihdfwsmruicgbj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Y1mJWKBYEScbYcv3LZUfHg_tI8rgmkY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("app.js berhasil dimuat");
+
+const registerForm = document.getElementById("registerForm");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        console.log("Tombol Buat Akun ditekan");
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const message = document.getElementById("message");
+
+        message.textContent = "Mendaftarkan akun...";
+
+        const { data, error } = await supabaseClient.auth.signUp({
+            email: email,
+            password: password
+        });
+
+        if (error) {
+            message.textContent = "Gagal: " + error.message;
+            console.error(error);
+            return;
+        }
+
+        message.textContent = "Pendaftaran berhasil! Silakan cek email untuk verifikasi.";
+        console.log("Pendaftaran berhasil", data);
+
+    });
+} else {
+    console.error("Register form tidak ditemukan!");
+}
